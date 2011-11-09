91494b9deacc647516227d3f8860825bd4b39a57
Julia <juliafanny@gmail.com>
2011-11-09 22:29:27 +0100

    lite enkla  views och routes.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,23 @@
 class CoursesController < ApplicationController
+	  def index
+    # @search = Project.search do
+    #   fulltext params[:search]
+    # end
+    # @projects = @search.results
+   @courses = Course.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @courses }
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @course }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Studentmap::Application.routes.draw do
   root :to => "sessions#new"
-  
+
   get "sign_up" => "users#new", :as => "sign_up"
   get "log_in" => "sessions#new", :as => "log_in"
   delete "log_out" => "sessions#destroy", :as => "log_out"
 
+  resources :sessions, :only => [:new, :create, :destroy]
+
   resources :users
 
-  resources :sessions, :only => [:new, :create, :destroy]
+  resources :courses do
+		resources :lessons
+	end
+
+  resources :locations
+
+  
 end
