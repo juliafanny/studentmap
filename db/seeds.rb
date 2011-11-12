fbce04f3ef1d2957913713bb7a6b0059f522db0a
zoee <zoee.silcock@chas.se>
2011-11-12 23:00:28 +0100

    added a prototype mobile app added some seed data cleared out solr temporary files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+School.create [{:name => 'KTH'}, {:name => 'SU'}]
+
+KTH = School.first
+SU = School.last
+
+SU.buildings.create({:name => 'Arrhenius', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'Biblioteket', :latitude => nil, :longditude => nil})
+a = SU.buildings.create({:name => 'Sodrahuset A', :latitude => 59.36277149185905, :longditude => 18.058784902095795})
+b = SU.buildings.create({:name => 'Sodra huset B', :latitude => 59.36275030559559, :longditude => 18.059472888708115})
+SU.buildings.create({:name => 'Sodra huset C', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'Sodra huset D', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'Sodra huset E', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'Sodra huset F', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS A', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS C', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS D', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS E', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS G', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS H', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS J', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS O', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS Q', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS T', :latitude => nil, :longditude => nil})
+SU.buildings.create({:name => 'K- HUS Z', :latitude => nil, :longditude => nil})
+
+a.rooms.create({:name => 'A1'});
+a.rooms.create({:name => 'A2'});
+a.rooms.create({:name => 'A3-U-N'});
+
+b.rooms.create({:name => 'B1'});
+b.rooms.create({:name => 'B2'});
+b.rooms.create({:name => 'B3'});
